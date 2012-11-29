49c854ee4d5fe39dd0ff82036b60e3925fd671d3
Richard Bolt <richard@richardbolt.com>
2012-11-29 00:04:41 -0800

    We're using virtualenv - these sys.path mods are unnecessary.

--- manage.py
+++ manage.py
@@ -2,27 +2,6 @@
 import os
 import sys
 
-try:
-    import mptt
-except ImportError:
-    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../django-mptt')))
-try:
-    import feincms
-except ImportError:
-    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../feincms')))
-try:
-    import form_designer
-except ImportError:
-    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../form_designer')))
-try:
-    import markupmirror
-except ImportError:
-    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../django-markupmirror')))
-try:
-    import elephantblog
-except ImportError:
-    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../feincms-elephantblog')))
-
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rboltphoto.settings")
