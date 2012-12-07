e64b525bcb68790adc6652d01ae959f2c02a1cfa
Richard Bolt <richard@richardbolt.com>
2012-12-06 22:52:57 -0800

    More flexibility to the template theme include code.

--- photographer/templatetags/rb_theme_tags.py
+++ photographer/templatetags/rb_theme_tags.py
@@ -6,30 +6,35 @@ from django.conf import settings
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
-def rb_theme_includes(context):
+def rb_theme_include(context, template_name):
     """
     Usage:
-    {% rb_theme_includes %}
-    
-    Adds a file named `include.html` from the current template directory. It
-    is parsed like any other Django template and popped right into the
-    calling template (usually the base template base.html).
-    
-    This is used to include extra head items like stylesheets and javascript
-    libraries specific to a given theme for more customization than pure css
+    {% rb_include '_footer.html' %}
+
+    Adds a file named `template_name` (`_footer.html` in the example) from
+    the current template directory. It is parsed like any other Django
+    template and popped right into the calling template (usually the base
+    template base.html). As a fallback, if the `template_name` does not
+    exist in the theme root directory, then the root template directory is
+    searched.
+
+    This is used to allow a given theme for more customization than pure css
     would allow.
-    
+
     """
     content = ''
+    root_file = os.path.abspath(os.path.join(os.path.dirname(__file__),
+            '..', 'templates', template_name))
     include_file = os.path.abspath(os.path.join(os.path.dirname(__file__),
-            '..', 'static', 'themes', context['RB_THEME'], 'include.html'))
+            '..', 'static', 'themes', context['RB_THEME'],
+            template_name))
     if os.path.isfile(include_file):
         tpl = template.Template(open(include_file).read())
         content = tpl.render(context)
+    elif os.path.isfile(root_file):
+        tpl = template.Template(open(root_file).read())
+        content = tpl.render(context)
     return content
-    
-    
-    
-    
-    
+
